affb16807e6802b9bc35e7e12d78b81750c1d0e3
soeatu <soeatsu@gmail.com>
2023-07-23 01:07:55 +0900

    siliarizaers & views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile, Post, Comment
 
-# Create your views here.
+#新規ユーザー作成に特化したAPI
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+#プロフィールに特化したAPI
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    #新規でユーザープロフィールを作成
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+#プロフィール取得に特化したAPI
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+#ポストに特化したAPI
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    #投稿を新規で作成する場合ユーザーを割りつける
+    def perform_create(self, serializer):
+        serializer.save(userPost=self.request.user)
+#コメントに特化したAPI
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userComment=self.request.user)
